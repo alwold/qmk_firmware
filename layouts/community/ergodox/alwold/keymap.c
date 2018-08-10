25c0f14cfd77758bf407dcbbbf2b59e77b12abb4
Al Wold <a@trusona.com>
2018-08-10 10:04:37 -0700

    Add tap dance thing to allow double tap of shift to caps lock

--- layouts/community/ergodox/alwold/keymap.c
+++ layouts/community/ergodox/alwold/keymap.c
@@ -6,6 +6,16 @@
 #define SYMB 1 // symbols
 #define MDIA 2 // media keys
 
+enum {
+  TD_LSFT_CAPS = 0,
+  TD_RSFT_CAPS = 1
+};
+
+qk_tap_dance_action_t tap_dance_actions[] = {
+  [TD_LSFT_CAPS]  = ACTION_TAP_DANCE_DOUBLE(KC_LSFT, KC_CAPS),
+  [TD_RSFT_CAPS]  = ACTION_TAP_DANCE_DOUBLE(KC_RSFT, KC_CAPS)
+};
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /* Keymap 0: Basic layer
  *
@@ -15,7 +25,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * | \      |   Q  |   W  |   E  |   R  |   T  | LCAG |           |  [   |   Y  |   U  |   I  |   O  |   P  |   ]    |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
  * | Tab    |   A  |   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  |   ;  |   '    |
- * |--------+------+------+------+------+------|  L2  |           | L1   |------+------+------+------+------+--------|
+ * |--------+------+------+------+------+------| MDIA |           | SYMB |------+------+------+------+------+--------|
  * | LShift |   Z  |   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |   /  | RShift |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
  *   | LGui | Grv  |Insert| Left | Right|                                       | Left | Down |  Up  | Right|  RGui |
@@ -35,7 +45,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_EQL,   KC_1,    KC_2,    KC_3,     KC_4,     KC_5,  KC_ESC,
         KC_BSLS,   KC_Q,    KC_W,    KC_E,     KC_R,     KC_T,  LCAG(KC_NO),
         KC_TAB,  KC_A,    KC_S,    KC_D,     KC_F,     KC_G,
-        KC_LSFT,  KC_Z,    KC_X,    KC_C,     KC_V,     KC_B,  LT(MDIA, KC_NO),
+        TD(TD_LSFT_CAPS),  KC_Z,    KC_X,    KC_C,     KC_V,     KC_B,  LT(MDIA, KC_NO),
         KC_LGUI,   KC_GRV,  KC_INS,  KC_LEFT,  KC_RGHT,
                                                KC_LCTL,KC_LALT,
                                                        KC_HOME,
@@ -44,7 +54,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
              KC_RGHT,     KC_6,   KC_7,   KC_8,   KC_9,   KC_0,     KC_MINS,
              KC_LBRC,    KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,     KC_RBRC,
                           KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,  KC_QUOT,
-             LT(SYMB, KC_NO),KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,  KC_RSFT,
+             LT(SYMB, KC_NO),KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,  TD(TD_RSFT_CAPS),
                                   KC_LEFT,KC_DOWN,KC_UP,  KC_RIGHT,  KC_RGUI,
              KC_RALT, KC_RCTL,
              KC_PGUP,
